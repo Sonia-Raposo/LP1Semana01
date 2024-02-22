3a5d301fda70dcb9ae1283f0eedfe5b422b7051d
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-22 19:09:14 +0000

    Added int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
