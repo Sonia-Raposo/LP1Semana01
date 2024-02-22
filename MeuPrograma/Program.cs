1b1f739a24941787c535d3eaf7af1fcaae75429e
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-22 17:50:58 +0000

    Added messages in tags

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Class Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Program prints 2 console messages
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
