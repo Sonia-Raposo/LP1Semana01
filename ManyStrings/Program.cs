3eb482c0017a8436726591b972fe999b8d419196
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-22 20:38:29 +0000

    Added string variables

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "I'm a string";
+            string b = "I'm a more complex string";
+            b = "Because I have changed";
+            string c = "I'm a string just \\chilling\\ here \u2746";
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
