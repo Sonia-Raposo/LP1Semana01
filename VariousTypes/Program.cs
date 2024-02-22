cb6da0774a0efb3be36a8b320baf4da354138cd1
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-22 19:57:43 +0000

    Aded real variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -14,9 +14,24 @@ namespace VariousTypes
             Console.WriteLine(b);
             Console.WriteLine(c);
 
-            char d = '\u2727';
+            char d = '\u2665';
+            char e = '\u2666';
+            char f = '\u2660';
+            char g = '\u2663';
 
             Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+
+            float h = 12.86f;
+            double i = 62.567;
+            decimal j = 7.777m;
+
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
         }
     }
 }
