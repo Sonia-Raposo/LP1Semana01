86bb5cdd3977ae940a03bb1731462a169907b32d
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-23 14:34:25 +0000

    Ask user for an int and save it

---
+++ AskForInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero inteiro:");
+            string valor = Console.ReadLine();
+
+
+            
+        }
+    }
+}
