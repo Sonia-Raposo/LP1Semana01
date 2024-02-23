427d04648d067ac3595509df5ec518ea4d31fea2
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-23 14:51:36 +0000

    Print sum of both numbers asked to the user

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,7 +7,14 @@ namespace AskForInput
         static void Main(string[] args)
         {
             Console.WriteLine("Insira um numero inteiro:");
-            string valor = Console.ReadLine();
+            int valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira um numero real:");
+            float real = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A soma dos números é {valor + real}");
+
+            
 
 
             
